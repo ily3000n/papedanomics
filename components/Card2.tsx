1540b5a5c04d28bced77fd766b57ac3cc489d10a
Muhammad <muhammadfabil40@gmail.com>
2024-08-03 16:37:11 +0700

    .

--- components/Card2.tsx
+++ components/Card2.tsx
@@ -23,7 +23,7 @@ const Card2 = () => {
               <button 
                 onClick={() => window.location.href = wisata.link ?? '#'} // Default link in case link is undefined
                 className="mt-2 p-2 inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                VISIT {wisata.title?.toUpperCase() ?? 'UNKNOWN'}
+                VISIT {wisata.title?.toUpperCase() ?? 'UNKNOWN'} 
               </button>
             </div>
           </div>
