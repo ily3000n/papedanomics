2d6543e39527c319f25dc039c691acb47da6de1d
Muhammad <muhammadfabil40@gmail.com>
2024-08-09 06:15:05 +0700

    vercell analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "400", "600"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
