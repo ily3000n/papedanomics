42b341ddf1453926fa8b3b7f27ca687dc8082bd7
Muhammad <muhammadfabil40@gmail.com>
2024-08-09 06:17:38 +0700

    speedinsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({ subsets: ["latin"], weight: ["300", "400", "600"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>{children}</body>
       <Analytics />
+      <SpeedInsights/>
     </html>
   );
 }
